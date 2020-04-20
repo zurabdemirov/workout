c5b787b67125177362e093905d0038b5e4788e91
roman.bednyakov@sharp-dev.net <roman.bednyakov@sharp-dev.net>
2020-04-20 18:08:35 +0400

    new

--- src/features/Auth/pages/Example/index.tsx
+++ src/features/Auth/pages/Example/index.tsx
@@ -9,31 +9,62 @@ interface Example {
     startLoading: (data: boolean) => void;
     stopLoading: (data: boolean) => void;
     loading: boolean;
+    user: {
+        name: string;
+        email: string;
+    };
+    value: string;
 }
 
 interface IState {
     exampleReducer: {
         loading: boolean;
+        user: {
+            name: string;
+            email: string;
+        };
+        value: string;
     }
 }
 
 const Example = (props: Example) => {
-    // const startComponent = () => {
-    //     props.startLoading(true);
-    //     setTimeout(() => {
-    //         props.stopLoading(false);
-    //     }, 3000);
-    // }
+    const start = () => {
+        props.startLoading(true);
+        setTimeout(() => {
+            props.stopLoading(false);
+        }, 3000);
+    }
+
+
+
+    const onSubmit = () => {
+        console.log(11111)
+    }
+    
+    const onchangeName = (e: any) => {
+        console.log(e);
+    }
+
     return (
         <div className={style.App}>
+            <button onClick={start}>startLoading</button>
             {props.loading ? 'Loading...' : 'Start'}
+
+            <input type = "text" value = {props.value} onChange = {onchangeName}
+            />
+            {/* <input type="email" onChange={onchangeEmail}/> */}
         </div>
     );
 }
+
 export default connect(
-    (state: IState) => ({
-        loading: state.exampleReducer.loading,
-    }),
+    (state: IState) => {
+        console.log(state);
+        return {
+            loading: state.exampleReducer.loading,
+            user: state.exampleReducer.user,
+        }
+    },
     (dispatch) => ({
         startLoading: (data: boolean) => dispatch(startLoading(data)),
         stopLoading: (data: boolean) => dispatch(stopLoading(data)),
--- src/features/Auth/store/index.ts
+++ src/features/Auth/store/index.ts
@@ -2,21 +2,37 @@ import {createAction, createReducer} from '@reduxjs/toolkit'
 
 interface initialState {
     loading: boolean;
+    user: {
+        name: string;
+        email: string;
+    };
 }
 
 export const startLoading = createAction<boolean, 'startLoading'>('startLoading')
 export const stopLoading = createAction<boolean, 'stopLoading'>('stopLoading')
+export const updateUser = createAction<object, 'updateUser'>('updateUser')
+
 const initialState: initialState = {
     loading: false,
+    user: {
+        name: '',
+        email: '',
+    },
 };
+
 const exampleReducer = createReducer(initialState, {
     [startLoading.type]: (state, action) => {
-        const loading = action
+        const loading = action.payload
         return {...state, loading}
     },
     [stopLoading.type]: (state, action) => {
-        const loading = action
+        const loading = action.payload
         return {...state, loading}
-    }
+    },
+    [updateUser.type]: (state, action) => {
+        const user = action.payload
+        return {...state, user}
+    },
 })
+
 export default exampleReducer;
